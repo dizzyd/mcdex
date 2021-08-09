ed5cd72b47168a3aeb5bc6786a98dc049c810f0f
Dave Smith <dizzyd@dizzyd.com>
2021-08-09 08:19:37 -0600

    Add text-based mod explorer

---
+++ pkg/ui/explorer.go
@@ -0,0 +1,56 @@
+package ui
+
+import (
+	"fmt"
+	"github.com/pkg/browser"
+	"github.com/rivo/tview"
+	"mcdex/pkg"
+)
+
+type Explorer struct {
+	app *tview.Application
+	db *pkg.Database
+
+	modBrowser *ModBrowser
+
+	pages *tview.Pages
+}
+
+func NewExplorer(db *pkg.Database) (*Explorer, error) {
+	var err error
+	e := &Explorer{db: db, app: tview.NewApplication()}
+
+	e.modBrowser, err = NewModBrowser(e.app, db)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing mod browser: %+v", err)
+	}
+	e.modBrowser.SetModSelectedFunc(e.showModDetail)
+
+	e.pages = tview.NewPages().
+		AddPage("mod_browser", e.modBrowser.RootView(), true, true)
+
+	e.app.SetRoot(e.pages, true)
+
+	return e, nil
+}
+
+func (e *Explorer) Run() error {
+	return e.app.Run()
+}
+
+func (e *Explorer) showModDetail(slug string, loader string, mcvsn string) {
+	url := fmt.Sprintf("https://www.curseforge.com/minecraft/mc-mods/%s", slug)
+	browser.OpenURL(url)
+}
+
+func makeCenteredModal(p tview.Primitive, width, height int) tview.Primitive {
+	// Generate a new flex that has 3 columns, with center column "width" wide; within the centered
+	// column, generate ANOTHER new flex by rows that has the center row "height" tall
+	return tview.NewFlex().
+		AddItem(nil, 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(nil, 0, 1, false).
+			AddItem(p, height, 1, false).
+			AddItem(nil, 0, 1, false), width, 1, false).
+		AddItem(nil, 0, 1, false)
+}
---
+++ pkg/ui/mod_browser.go
@@ -0,0 +1,146 @@
+package ui
+
+import (
+	"fmt"
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+	"mcdex/pkg"
+	"strings"
+)
+
+type ModSelectedHandler func(slug string, loader string, mcvsn string)
+
+type ModBrowser struct {
+	app *tview.Application
+	db *pkg.Database
+
+	table *tview.Table
+	loaderDropDown *tview.DropDown
+	vsnDropDown *tview.DropDown
+
+	root tview.Primitive
+
+	focusOrder []tview.Primitive
+	focusIndex int
+
+	forgeMcVersions []string
+	fabricMcVersions []string
+
+	loader string
+	mcvsn string
+
+	onModSelected ModSelectedHandler
+}
+
+func NewModBrowser(app *tview.Application, db *pkg.Database) (*ModBrowser, error) {
+	forgeMcVersions, err := db.GetSupportedMCVersions("forge")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get supported MC version for Forge: %+v", err)
+	}
+
+	fabricMcVersions, err := db.GetSupportedMCVersions("fabric")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get support MC versions for Fabric: %+v", err)
+	}
+
+	b := &ModBrowser{
+		app: app,
+		db: db,
+		forgeMcVersions: forgeMcVersions,
+		fabricMcVersions: fabricMcVersions,
+	}
+
+	b.table = tview.NewTable().
+		SetBorders(false).
+		SetFixed(1, 1).
+		SetSelectable(true, false).
+		SetEvaluateAllRows(true).
+		SetSelectedFunc(b.modSelected).
+		SetDoneFunc(b.componentDone)
+
+	b.vsnDropDown = tview.NewDropDown().
+		SetLabel("Version:").
+		SetDoneFunc(b.componentDone)
+
+	b.loaderDropDown = tview.NewDropDown().
+		SetLabel("Loader:").
+		SetOptions([]string{"Forge", "Fabric"}, b.loaderSelected).
+		SetCurrentOption(0).
+		SetDoneFunc(b.componentDone)
+
+	b.vsnDropDown.SetBorder(true)
+	b.loaderDropDown.SetBorder(true)
+
+	b.focusOrder = []tview.Primitive{b.loaderDropDown, b.vsnDropDown, b.table}
+	b.focusIndex = 0
+
+	b.root = tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(tview.NewFlex().
+			AddItem(b.loaderDropDown, 0, 1, true).
+			AddItem(b.vsnDropDown, 0, 1, true),
+			0, 1, true).
+		AddItem(b.table, 0, 10, true)
+
+	return b, nil
+}
+
+func (b *ModBrowser) SetModSelectedFunc(f ModSelectedHandler) {
+	b.onModSelected = f
+}
+
+func (b *ModBrowser) RootView() tview.Primitive {
+	return b.root
+}
+
+func (b *ModBrowser) loaderSelected(name string, index int) {
+	b.loader = strings.ToLower(name)
+	b.refreshVersions()
+}
+
+func (b *ModBrowser) versionSelected(name string, index int) {
+	b.mcvsn = name
+	b.refreshTable()
+}
+
+func (b *ModBrowser) modSelected(row, column int) {
+	slug := b.table.GetCell(row, 0).Text
+	if b.onModSelected != nil {
+		b.onModSelected(slug, b.loader, b.mcvsn)
+	}
+}
+
+func (b *ModBrowser) componentDone(key tcell.Key) {
+	if key == tcell.KeyTab {
+		b.focusIndex = (b.focusIndex+1) % len(b.focusOrder)
+		b.app.SetFocus(b.focusOrder[b.focusIndex])
+	}
+}
+
+func (b *ModBrowser) refreshVersions() {
+	if b.loader == "forge" {
+		b.vsnDropDown.SetOptions(b.forgeMcVersions, b.versionSelected)
+	} else {
+		b.vsnDropDown.SetOptions(b.fabricMcVersions, b.versionSelected)
+	}
+	b.vsnDropDown.SetCurrentOption(0)
+}
+
+func (b *ModBrowser) refreshTable() {
+	row := 1
+	printer := message.NewPrinter(language.English)
+
+	b.table.SetCell(0, 0, tview.NewTableCell("Slug").SetSelectable(false))
+	b.table.SetCell(0, 1, tview.NewTableCell("Downloads").SetSelectable(false))
+	b.table.SetCell(0, 2, tview.NewTableCell("Desc").SetSelectable(false))
+
+	b.db.ForEachMod(b.mcvsn, b.loader, func(id int, slug string, description string, downloads int) error {
+		b.table.SetCell(row, 0, tview.NewTableCell(slug).SetMaxWidth(25))
+		b.table.SetCell(row, 1, tview.NewTableCell(printer.Sprintf("%d", downloads)))
+		b.table.SetCell(row, 2, tview.NewTableCell(description).SetMaxWidth(150))
+		row++
+		return nil
+	})
+}
